d09744c07651a21c4bc17e5fa73c681ed2c8ba56
yjh2848 <yunj05763@gmail.com>
2023-11-20 16:28:15 +0900

    add : metadata setting

---
+++ seo.config.tsx
@@ -0,0 +1,16 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "INFO.GG",
+  description: "발로란트 정보 공유 사이트",
+  openGraph: {
+    title: "INFO.GG",
+    description: "발로란트 정보 공유 사이트",
+    url: "아직 없음",
+    siteName: "INFO.GG",
+  },
+  viewport: {
+    width: "device-width",
+    initialScale: 1,
+  },
+};
